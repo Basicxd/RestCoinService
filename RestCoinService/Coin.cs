2a7bc2657e369b8c0e9a3c7e90df0aa5dd378baf
basicxd <mikail-fener@hotmail.com>
2019-04-03 09:10:21 +0200

    Shieet

---
+++ RestCoinService/Coin.cs
@@ -0,0 +1,10 @@
+﻿namespace RestCoinService
+{
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string Genstand { get; set; }
+        public int Bud { get; set; }
+        public string Navn { get; set; }
+    }
+}
